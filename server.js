99c01288f5ebe2466d9e636c754b98bccb3c009a
NoeOn <alxg5516@gmail.com>
2023-09-08 21:55:38 -0600

    Implementando la ruta /login y un nuevo middleWare.js para manerar en jsonwebtoken

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 const bcrypt = require('bcrypt');
 
 
 const connection = require('./database');
 const emailConfig = require('./emailConfig');
+const middleWare = require('./middleWare');
 
 
 const isStrongPassword = (password) => {
@@ -237,6 +240,57 @@ app.get('/verify', async (req, res) => {
     } catch (error) {
         console.error('Error interno en el servidor:', error);
         res.status(500).json({ error: 'Ocurrió un error durante la verificación de correo. Por favor, inténtelo de nuevo más tarde.' });
+    }
+});
+
+//Inicio de sesión para los usuarios
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // Consultar la base de datos para obtener el usuario por su correo electrónico
+        const query = 'SELECT * FROM users WHERE email = ?';
+        const results = await new Promise((resolve, reject) => {
+            connection.query(query, [email], (err, results) => {
+                if (err) {
+                    console.error('Error querying the database:', err);
+                    reject(err);
+                }
+                resolve(results);
+            });
+        });
+
+        if (results.length === 0) {
+            return res.status(401).json({ error: 'No hemos podido autenticar su acceso' });
+        }
+
+        const user = results[0];
+
+        if (!user.verified) {
+            return res.status(401).json({ error: 'Por favor, verifique su cuenta antes de iniciar sesión' });
+        }
+
+        // Comparar la contraseña ingresada con el hash almacenado en la base de datos
+        const isMatch = await new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (compareErr, isMatch) => {
+                if (compareErr) {
+                    console.error('Error comparing passwords:', compareErr);
+                    reject(compareErr);
+                }
+                resolve(isMatch);
+            });
+        });
+
+        if (!isMatch) {
+            return res.status(401).json({ error: 'No hemos podido autenticar su acceso' });
+        }
+
+        // Generar un token y agregarlo a la respuesta
+        const token = middleWare.generateToken(user.id);
+        res.status(200).json({ message: 'Datos de acceso validados satisfactoriamente', token });
+    } catch (error) {
+        console.error('Error interno en el servidor:', error);
+        res.status(500).json({ error: 'Ocurrió un error durante el proceso de inicio de sesión. Por favor, inténtelo de nuevo más tarde.' });
     }
 });
 
